3cc40130e0d2dc3cc2edc2c5fbab2ecbc9464723
Daniel Gazzoli Nunes <danielgn91@gmail.com>
2026-06-15 00:33:47 -0300

    fix: include EntryTag model in models __init__ for table creation

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,4 +3,5 @@ from .transaction import Transaction
 from .entry import Entry
 from .tag import Tag
 from .account_tag import AccountTag
+from .entry_tag import EntryTag
 from .enums import TransactionStatus, AccountType, NormalBalance
